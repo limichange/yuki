7410d60db0fb943347a59e87fe317fba84c49fcf
limichange <limichange@hotmail.com>
2016-10-20 17:51:04 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <yuki-draggable>test</yuki-draggable>
+  </div>
+</template>
+
+<script>
+import YukiDraggable from './YukiDraggable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    YukiDraggable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/YukiDraggable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'YukiDraggable',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .main {
+    background: red;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+</style>
