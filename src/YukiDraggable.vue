bfdfe7b0c232e2774adf01f999c950eb6408fb2e
limichange <limichange@hotmail.com>
2016-10-21 11:19:17 +0800

    update style

--- src/YukiDraggable.vue
+++ src/YukiDraggable.vue
@@ -5,6 +5,20 @@
        v-bind:style="styleObject"
        class="main-div">
     <slot></slot>
+
+    <div class="line left"></div>
+    <div class="line right"></div>
+    <div class="line top"></div>
+    <div class="line bottom"></div>
+
+    <div class="point left top"></div>
+    <div class="point top center"></div>
+    <div class="point right top"></div>
+    <div class="point left middle"></div>
+    <div class="point right middle"></div>
+    <div class="point left bottom"></div>
+    <div class="point center bottom"></div>
+    <div class="point right bottom"></div>
   </div>
 </template>
 
@@ -19,14 +33,14 @@
           height: '100px',
           left: '100px',
           top: '100px',
-          background: '#eeeeee'
+          background: '#00A1CA'
         }
       }
     },
-
     methods: {
       mousedown (event) {
         console.log('mousedown')
+        event.target.classList.add('draging')
       },
       mousemove (event) {
         this._generatePosition(event)
@@ -34,6 +48,7 @@
       },
       mouseup (event) {
         console.log('mouseup')
+        event.target.classList.remove('draging')
       },
       _generatePosition (event) {
         var pageX = event.pageX
@@ -45,7 +60,46 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .line {
+    width: 100%;
+  }
+
+  $point-width: 20px;
+  $point-height: 20px;
+
+  $point-width-half: $point-width / 2;
+  $point-height-half: $point-height / 2;
+
+  .point {
+    position: absolute;
+    width: $point-width;
+    height: $point-height;
+    background: blue;
+    border-radius: $point-width;
+
+    &.top {
+      top: - $point-height-half;
+    }
+    &.bottom {
+      bottom: - $point-height-half;
+    }
+    &.left {
+      left: - $point-width-half;
+    }
+    &.right {
+      right: - $point-width-half;
+    }
+    &.middle {
+      top: 50%;
+      margin-top: - $point-height-half;
+    }
+    &.center {
+      left: 50%;
+      margin-left: - $point-width-half;
+    }
+  }
+
   .main-div {
 
   }
